78eeb2cf3100de03984009cae8149dd08335f6ef
skematiko-dev <143404844+skematiko-dev@users.noreply.github.com>
2023-11-27 17:28:18 +0530

    Update _menu.php
    dektrium-dev/yii2-rbac to skematiko-dev/yii2-rbac

--- views/admin/_menu.php
+++ views/admin/_menu.php
@@ -26,17 +26,17 @@ use yii\bootstrap4\Nav;
         [
             'label' => Yii::t('user', 'Roles'),
             'url' => ['/rbac/role/index'],
-            'visible' => isset(Yii::$app->extensions['dektrium/yii2-rbac']),
+            'visible' => isset(Yii::$app->extensions['skematiko-dev/yii2-rbac']),
         ],
         [
             'label' => Yii::t('user', 'Permissions'),
             'url' => ['/rbac/permission/index'],
-            'visible' => isset(Yii::$app->extensions['dektrium/yii2-rbac']),
+            'visible' => isset(Yii::$app->extensions['skematiko-dev/yii2-rbac']),
         ],
         [
             'label' => \Yii::t('user', 'Rules'),
             'url'   => ['/rbac/rule/index'],
-            'visible' => isset(Yii::$app->extensions['dektrium/yii2-rbac']),
+            'visible' => isset(Yii::$app->extensions['skematiko-dev/yii2-rbac']),
         ],
         [
             'label' => Yii::t('user', 'Create'),
@@ -48,17 +48,17 @@ use yii\bootstrap4\Nav;
                 [
                     'label' => Yii::t('user', 'New role'),
                     'url' => ['/rbac/role/create'],
-                    'visible' => isset(Yii::$app->extensions['dektrium/yii2-rbac']),
+                    'visible' => isset(Yii::$app->extensions['skematiko-dev/yii2-rbac']),
                 ],
                 [
                     'label' => Yii::t('user', 'New permission'),
                     'url' => ['/rbac/permission/create'],
-                    'visible' => isset(Yii::$app->extensions['dektrium/yii2-rbac']),
+                    'visible' => isset(Yii::$app->extensions['skematiko-dev/yii2-rbac']),
                 ],
                 [
                     'label' => \Yii::t('user', 'New rule'),
                     'url'   => ['/rbac/rule/create'],
-                    'visible' => isset(Yii::$app->extensions['dektrium/yii2-rbac']),
+                    'visible' => isset(Yii::$app->extensions['skematiko-dev/yii2-rbac']),
                 ]
             ],
         ],
